a26764dc9d719ae6f5cea95de37d99bcf6e50597
Justin Holt <justinvholt@gmail.com>
2019-02-16 00:59:02 -0600

    Added bio page and essay

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get "/:bio", to: "pages#bio"
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  def bio
-  end
 end
