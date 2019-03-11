b28e0efde844c11f85a8e1cf69527bd6a59b1299
Justin Holt <justinvholt@gmail.com>
2019-03-11 18:57:17 -0500

    Final cleanups

--- app/javascript/gotobyscroll.js
+++ app/javascript/gotobyscroll.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $('a').click(function(){ goToByScroll($(this).attr('href')); return false; });
+  $("a").click(function(){ goToByScroll($(this).attr("href")); return false; });
 });
 
 function goToByScroll(id){
-  $('html,body').animate({scrollTop: $(id).offset().top - 55}, 1500);
+  $("html,body").animate({scrollTop: $(id).offset().top - 55}, 1500);
 }
