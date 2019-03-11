0a0afecc671300e44c35bada9f834040caa14cb6
Justin Holt <justinvholt@gmail.com>
2019-03-11 02:58:31 -0500

    Fader on page load

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,3 @@ import "bootstrap";
 import "contactform";
 import "custom";
 import "gotobyscroll";
-// import "animsition";
-// import "resetform";
-// import "turbolinks-animate";
--- app/javascript/resetform.js
+++
@@ -1,7 +0,0 @@
-function resetForm() {
-  document.querySelectorAll(".form-field").reset();
-}
-
-$(document).submit(function() {
-  return resetForm();
-});
